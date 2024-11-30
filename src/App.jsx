4cf2e209da5cceabb21ba48e4a034d964b9a18fc
Niraj Gupta <coder4117@gmail.com>
2024-11-30 19:32:01 +0530

    Griid tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ function App() {
 
   return (
     <>
-      <div className='flex justify-center'>
-        <h1 className='bg-red-600'>Niraj</h1>
-        <h1 className='bg-yellow-600'>Niraj</h1>
-        <h1 className='bg-blue-600'>Niraj</h1>
+      <div className='grid grid-cols-10'>
+        <h1 className='bg-red-600 col-span-4'>Niraj</h1>
+        <h1 className='bg-yellow-600 col-span-4'>Niraj</h1>
+        <h1 className='bg-blue-600 col-span-2'>Niraj</h1>
       </div>
     </>
   )
