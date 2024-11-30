5b88fd8f1ebdce791ea92d30d1d3277b9e1de496
Niraj Gupta <coder4117@gmail.com>
2024-11-30 19:23:18 +0530

    Flex complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ function App() {
 
   return (
     <>
-      
+      <div className='flex justify-center'>
+        <h1 className='bg-red-600'>Niraj</h1>
+        <h1 className='bg-yellow-600'>Niraj</h1>
+        <h1 className='bg-blue-600'>Niraj</h1>
+      </div>
     </>
   )
 }
